c60a5a75e0c8dbb40b9377d64a324e0727aecde8
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-05 22:38:34 -0700

    feat: set up local pote

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static("./public"));
+
+app.listen(5000, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("working!");
+  }
+})
