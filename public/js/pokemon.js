35c023d61d79512a793d0bc1e90a2d02705010ab
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-08 03:44:16 -0700

    style: set background-colour of pokemon card

--- public/js/pokemon.js
+++ public/js/pokemon.js
@@ -5,6 +5,52 @@ function pokemonCard(data){
   $("img").attr("src", data.sprites.other.dream_world.front_default);
   $(".length").html(`Length: ${data.height / 10}m`);
   $(".weight").html(`Weight: ${data.weight / 10}kg`);
+
+  let type = data.types[0].type.name
+  if (type == "fire") {
+    $(".card-row").css("background-color", "#FDDFDF");
+  }
+  else if (type == "grass") {
+    $(".card-row").css("background-color", "#DEFDE0");
+  }
+  else if (type == "electric") {
+    $(".card-row").css("background-color", "#FCF7DE");
+  }
+  else if (type == "water") {
+    $(".card-row").css("background-color", "#DEF3FD");
+  }
+  else if (type == "ground") {
+    $(".card-row").css("background-color", "#f4e7da");
+  }
+  else if (type == "rock") {
+    $(".card-row").css("background-color", "#d5d5d4");
+  }
+  else if (type == "fairy") {
+    $(".card-row").css("background-color", "#fceaff");
+  }
+  else if (type == "poison") {
+    $(".card-row").css("background-color", "#98d7a5");
+  }
+  else if (type == "bug") {
+    $(".card-row").css("background-color", "#f8d5a3");
+  }
+  else if (type == "dragon") {
+    $(".card-row").css("background-color", "#97b3e6");
+  }
+  else if (type == "psychic") {
+    $(".card-row").css("background-color", "#eceda1");
+  }
+  else if (type == "flying") {
+    $(".card-row").css("background-color", "#F5F5F5");
+  }
+  else if (type == "fighting") {
+    $(".card-row").css("background-color", "#E6E0D4");
+  }
+  else if (type == "normal") {
+    $(".card-row").css("background-color", "#F5F5F5");
+  } else {
+    console.log(type);
+  }
 }
 
 $(document).ready(() => {
