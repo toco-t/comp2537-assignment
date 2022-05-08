97f3b785653ab4b3e85b52e71399581e1362d008
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-07 19:29:09 -0700

    feat: display random pokemons dynamically on index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,19 @@
-function process(data) {
-  first = ""
-  second = ""
-  third = ""
+function randomPokemons() {
+  for (let i = 0; i < 9; i++) {
+    let random_num = Math.floor(Math.random() * 151) + 1
 
-  for (let i = 0; i < 3; i++) {
-    var random_num = Math.random() * 151 + 1
-    first += `<div class="pokemon"><p>No. ${i}</p>${data.results[i].name}</div>`
-  }
-  $(".first").html(first);
+    let poke_img = `<div class="pokemon"><p>No. ${random_num}</p><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${random_num}.gif"></img></div>`
 
-  for (let i = 3; i < 6; i++) {
-    second += `<div class="pokemon"><p>No. ${i}</p>${data.results[i].name}</div>`
+    if (i < 3) {
+      $(".first").append(poke_img)
+    } else if (i < 6) {
+      $(".second").append(poke_img)
+    } else {
+      $(".third").append(poke_img)
+    }
   }
-  $(".second").html(second);
-
-  for (let i = 6; i < 9; i++) {
-    third += `<div class="pokemon"><p>No. ${i}</p>${data.results[i].name}</div>`
-  }
-  $(".third").html(third);
 }
 
-function randomPokemon() {
-  $.ajax({
-    url: "https://pokeapi.co/api/v2/pokemon",
-    type: "GET",
-    success: process
-  })
-}
-
-$(document).ready(randomPokemon);
+$(document).ready(() => {
+  randomPokemons();
+});
