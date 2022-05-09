4a52394bbaa3a8e53834673b86520d11283d1a06
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-08 20:55:51 -0700

    feat: implement searching by name

--- public/js/search.js
+++ public/js/search.js
@@ -66,5 +66,35 @@ $("#name").change(function() {
   $(".second-row").html("");
   $(".third-row").html("");
 
-  pokemon_name = $("#name").val();
+  var pokemon_name = $("#name").val();
+
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/?limit=1500`,
+    type: "GET",
+    success: (data) => {
+      var filteredPokemons = []
+
+      for (let i = 0; i < data.results.length; i++) {
+        if (pokemon_name.includes(String(data.results[i].name[0]))) {
+          filteredPokemons.push(i);
+        }
+      }
+
+      for (let i = 0; i < 9; i++) {
+        let index = filteredPokemons[i];
+        let url = (data.results[index].url).split("/");
+        let id = url[6]
+
+        let filteredPokemon = `<div class="pokemon"><a href="pokemon.html?id=${id}"><p>No. ${id}</p><p>${data.results[index].name.toUpperCase()}</p></a></div>`
+
+        if (i < 3) {
+          $(".first-row").append(filteredPokemon);
+        } else if (i < 6) {
+          $(".second-row").append(filteredPokemon);
+        } else {
+          $(".third-row").append(filteredPokemon);
+        }
+      }
+    }
+  })
 })
