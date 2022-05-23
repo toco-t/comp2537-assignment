2d8250c5fbc34d7b05b30f4758e0f39cfca2d8b4
Toco Tachibana <ttachibana@my.bcit.ca>
2022-05-23 07:28:16 -0700

    refactor: remove extra conditions

--- public/js/account.js
+++ public/js/account.js
@@ -6,23 +6,19 @@ $(document).ready(
       $("#account__name").html(data.username);
       $("#account__id").html(data.user_id);
 
-      if (data.timeline[].length > 0) {
-        for (let i = 0; i < data.timeline.length; i++) {
-          $(".account__timeline").append(
-            `<div class="account__timeline__event"><p>${data.timeline[i].content}</p>
-            <p>Time: ${(data.timeline[i].time).split("GMT")[0]}</p></div>`
-          )
-        }
+      for (let i = 0; i < data.timeline.length; i++) {
+        $(".account__timeline").append(
+          `<div class="account__timeline__event"><p>${data.timeline[i].content}</p>
+          <p>Time: ${data.timeline[i].time.split("GMT")[0]}</p></div>`
+        )
       }
 
-      if (data.past_orders[].length > 0) {
-        for (let i = 0; i < data.past_orders.length; i++)
-          $(".account__past-orders").append(
-            `<div class="account__past-orders__order"><p>&#165;${data.past_orders[i].total}</p>
-            <p>Number of Pokemon: ${data.past_orders[i].quantity}</p>
-            <p>Time: ${(data.past_orders[i].time).split("GMT")[0]}</p></div>`
-          )
-        }
+      for (let i = 0; i < data.past_orders.length; i++) {
+        $(".account__past-orders").append(
+          `<div class="account__past-orders__order"><p>&#165;${data.past_orders[i].total}</p>
+          <p>Number of Pokemon: ${data.past_orders[i].quantity}</p>
+          <p>Time: ${data.past_orders[i].time.split("GMT")[0]}</p></div>`
+        )
       }
     }
   })
